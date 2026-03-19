833335081e14600f657472fb86308314e3086820
Joshua Sierra <joshuasierra@example.com>
2026-03-19 12:44:49 +0800

    asd

--- includes/forgot_password.php
+++ includes/forgot_password.php
@@ -1,8 +1,12 @@
 <?php
-// forgot_password.php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once '../config/db.php';
+
+// START SESSION HERE!
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
 <!DOCTYPE html>
 <html>
